5776182898456303be13595ec33f752a0b721832
Joedang <shields6@pdx.edu>
2017-08-07 12:29:20 -0700

    got the bot working; imposed artificial rate limit

--- ghostbot.py
+++ ghostbot.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 
 import praw
+import time
 
 bot= praw.Reddit(
         user_agent="ghostbot12 v0.1",
-        client_id="SPOOK12",
-        client_secret="superScarySpookr42069",
+        client_id="62c9efcMBeyfQw",
+        client_secret=open("secret.txt").read().replace("\n",""),
         username="ghostbot12",
         password= open("pass.txt").read().replace("\n", "")
         )
 
 subreddit= bot.subreddit('me_irl')
 comments= subreddit.stream.comments()
-SPOOKE_reply= open("genericSPOOK.txt").read()
+SPOOKE_reply= open("genericSPOOK.txt", encoding="UTF-8").read()
 
 for comment in comments:
     text = comment.body # Fetch body
@@ -20,4 +21,8 @@ for comment in comments:
     if 'ghost' in text.lower():
         # Generate a message
         message = SPOOKE_reply
+        print("about to SPOOP ", comment.author.name)
+        print("\n")
         comment.reply(message) # Send message
+        print("SPOOKD", comment.author.name, "\n")
+        time.sleep(60)
